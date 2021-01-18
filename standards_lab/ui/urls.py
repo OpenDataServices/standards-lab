f1d1467e9c6429a307ed880d4ccc08a2a4ca3cf8
Michael Wood <michael.wood@opendataservices.coop>
2021-01-18 18:32:48 +0000

    backup

---
+++ standards_lab/ui/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.views.generic.base import TemplateView
+
+from ui.apps import UiConfig
+
+app_name = UiConfig.name
+
+urlpatterns = [
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
+    path("about", TemplateView.as_view(template_name="about.html"), name="about")
+]
