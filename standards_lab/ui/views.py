e1531c8316331bab6b6df8184c7f79a2c58e3885
Michael Wood <michael.wood@opendataservices.coop>
2021-02-03 16:49:49 +0000

    ui: templates: Add validation of project name
    Rewrite the home page project creator area. This is done in jquery as it
    is a single button specific to the home page.
    
    Make sure that we can only create compatible project names with the
    project slugs. A global js object is created. Added to it is a regex
    for testing project names and the editMode variable between pages.
    
    Also remove a debug print from views.

--- standards_lab/ui/views.py
+++ standards_lab/ui/views.py
@@ -13,7 +13,6 @@ class Home(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["projects"] = os.listdir(settings.ROOT_PROJECTS_DIR)
-        print(context)
         return context
 
 
