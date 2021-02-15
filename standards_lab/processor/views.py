8e0e7c8a1967d88979c3c61f51b83a18bf0d345b
Michael Wood <michael.wood@opendataservices.coop>
2021-02-15 11:29:40 +0000

    WIP

--- standards_lab/processor/views.py
+++
@@ -1,9 +0,0 @@
-from django.http.response import JsonResponse
-from django.views.generic import View
-
-
-class JobStatus(View):
-    """ Shows all the jobs for this session """
-
-    def get(self, *args, **kwargs):
-        return JsonResponse({})
