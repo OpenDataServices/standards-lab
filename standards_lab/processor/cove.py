e52855c7f510f04058c29e02295b328fdbc337cb
Ben Webb <ben.webb@opendataservices.coop>
2021-02-25 17:52:49 +0000

    processor.cove: Move common_checks_context call into a lib_cove_wrapper
    (This wrapper is then the function that it enqueued.)
    
    https://github.com/OpenDataServices/standards-lab/issues/24

--- standards_lab/processor/cove.py
+++ standards_lab/processor/cove.py
@@ -20,7 +20,11 @@ from .extra_validator_funcs import patch_validator
 patch_validator(validator)
 
 
-def start(project):
+def lib_cove_wrapper(
+    project,
+    data_file,
+    cache=False,
+):
     schema_name = project["rootSchema"]
 
     print(project, flush=True)
@@ -37,10 +41,35 @@ def start(project):
     )
     print(schema_obj.pkg_schema_url, flush=True)
 
-    output = {}
-    for data_file in project["dataFiles"]:
+    with open(
+        os.path.join(project["path"], data_file)
+    ) as fp, tempfile.TemporaryDirectory() as upload_dir:
         context = {"file_type": "json"}
 
+        # upload_dir is only used to output files to (e.g. cell source map
+        # from flatten-tool, or a cache of the validation results).
+        try:
+            json_data = json.load(fp, parse_float=Decimal)
+        except json.JSONDecodeError:
+            output[data_file] = {
+                "status": "FAILED",
+                "error": "Could not decode as a json file",
+            }
+            return
+
+        common_checks_context(
+            upload_dir,
+            json_data,
+            schema_obj,
+            schema_name,
+            context,
+            cache=False,
+        )
+
+
+def start(project):
+    output = {}
+    for data_file in project["dataFiles"]:
         job_id = project["name"] + "_cove_results_" + data_file
         try:
             job = Job.fetch(job_id, connection=django_rq.get_connection())
@@ -55,33 +84,13 @@ def start(project):
         except NoSuchJobError:
             pass
 
-        with open(
-            os.path.join(project["path"], data_file)
-        ) as fp, tempfile.TemporaryDirectory() as upload_dir:
-            # upload_dir is only used to output files to (e.g. cell source map
-            # from flatten-tool, or a cache of the validation results).
-            try:
-                # Possibly we should do this in the worker for performance reasons
-                # Issue: https://github.com/OpenDataServices/standards-lab/issues/24
-                json_data = json.load(fp, parse_float=Decimal)
-            except json.JSONDecodeError:
-                output[data_file] = {
-                    "status": "FAILED",
-                    "error": "Could not decode as a json file",
-                }
-                continue
-
-            job = django_rq.enqueue(
-                common_checks_context,
-                upload_dir,
-                json_data,
-                schema_obj,
-                schema_name,
-                context,
-                cache=False,
-                job_id=job_id,
-            )
-            output[data_file] = {"status": "SUCCESS"}
+        job = django_rq.enqueue(
+            lib_cove_wrapper,
+            project,
+            data_file,
+            job_id=job_id,
+        )
+        output[data_file] = {"status": "SUCCESS"}
     return output
 
 
